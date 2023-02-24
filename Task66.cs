756639d16791dbe566f188bc30183a281b03c7ed
vs57github <123260473+vs57github@users.noreply.github.com>
2023-02-24 22:21:10 +0300

    Add files via upload

---
+++ Task66.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace SumOfNaturalNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int m, n, sum = 0;
+
+            Console.Write("Введите начальное число: ");
+            m = int.Parse(Console.ReadLine());
+
+            Console.Write("Введите конечное число: ");
+            n = int.Parse(Console.ReadLine());
+
+            for (int i = m; i <= n; i++)  // Цикл для подсчета суммы чисел от M до N 
+            {
+                sum += i;   // Добавляем текущий элемент в сумму  
+            }
+
+            Console.WriteLine($"Сумма натуральных чисел от {m} до {n} = {sum}");
+
+        }
+    }
+}
